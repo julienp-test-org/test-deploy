24613f9a9b737f71164a04b23664fc83c26340e3
Julien P <julien@gatsbyjs.com>
2021-04-15 18:54:36 +0200

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,7 +8,7 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people :SLLSLASD:LASJF)(()PDFLKAJFHDS*(UIUJFKSDFSDSDFSDF</h1>
+    <h1>Hi people :SLLSLASD:LASJF)(()PDFLKAJFHDS*(UIUJFKSDFSDSDFSD a1111F</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
     <StaticImage
